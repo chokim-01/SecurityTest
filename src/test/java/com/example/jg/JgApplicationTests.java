deea4ecf5e74de2dafa8f7c8a46ced7b130a58c3
chokim-01 <chokim159@naver.com>
2024-02-21 19:40:05 +0900

    Initial commit

---
+++ src/test/java/com/example/jg/JgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
