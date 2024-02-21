400c66fef7c0f6db0059ab2eb2e179eae965d5c8
chokim-01 <chokim159@naver.com>
2024-02-22 06:06:40 +0900

    feature/2

--- src/main/java/com/example/jg/domain/posts/Posts.java
+++ src/main/java/com/example/jg/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.example.jg.domain.posts;
 
+import com.example.jg.domain.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
