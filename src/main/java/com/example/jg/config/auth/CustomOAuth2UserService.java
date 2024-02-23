95d19bfdd2f72a5edb3fb55280c807c79d5bd0e5
chokim-01 <chokim159@naver.com>
2024-02-23 16:17:35 +0900

    naver login

--- src/main/java/com/example/jg/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/example/jg/config/auth/CustomOAuth2UserService.java
@@ -37,7 +37,7 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
         User user = saveOrUpdate(attributes);
         httpSession.setAttribute("user",new SessionUser(user));
 
-        return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey().toString())),attributes.getAttributes(),attributes.getNameAttributeKey());
+        return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())),attributes.getAttributes(),attributes.getNameAttributeKey());
     }
     private User saveOrUpdate(OAuthAttributes attributes){
         User user = userRepository.findByEmail(attributes.getEmail())
--- src/main/java/com/example/jg/web/IndexController.java
+++ src/main/java/com/example/jg/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.example.jg.web;
 
+import com.example.jg.config.auth.LoginUser;
+import com.example.jg.config.auth.dto.SessionUser;
 import com.example.jg.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -13,8 +15,10 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+        if(user != null)
+            model.addAttribute("userName",user.getName());
         return "index";
     }
 
